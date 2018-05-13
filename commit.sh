5fbf8be32ddc124a41b105afae8629ab0765a985
JW\JRW <jorwalk@gmail.com>
2018-05-13 14:00:00 +0100

    add titanic csv

--- commit.sh
+++ commit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
-commit_message="$1"
-git add . -A
-git commit -m "$commit_message"
-git push
+# commit_message="$1"
+# git add . -A
+# git commit -m "$commit_message"
+# git push
+d=2009-12-27
+while [ "$d" != 2011-01-01 ]; do 
+  echo $d
+  d=$(date -I -d "$d + 1 day")
+done
+# git config --global credential.helper wincred
