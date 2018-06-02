7f522015f1323ddf02cdd09a7ecfcaa3f030156b
JW\JRW <jorwalk@gmail.com>
2018-06-01 20:03:25 -0500

    new commit

--- commit.sh
+++ commit.sh
@@ -3,3 +3,8 @@ commit_message="$1"
 git add . -A
 git commit -m "$commit_message"
 git push
+# d=2009-12-27
+# while [ "$d" != 2011-01-01 ]; do 
+#   echo $d
+#   d=$(date -I -d "$d + 1 day")
+# done
